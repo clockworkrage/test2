fd620ddae9a99dd296705eb098a8e922ea98777a
clockworkrage <kamvalentin@gmail.com>
2018-04-17 11:39:37 +0300

    Вывод сообщения Test 10 раз

--- Main.java
+++ Main.java
@@ -2,7 +2,9 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello, World");
-        System.out.println("Test");
+        for (int i = 0; i < 10; i++) {
+        	System.out.println("Test");
+    	}
     }
 
 }
